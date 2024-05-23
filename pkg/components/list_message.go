7c2b493961969f71a6e13fe5243583d8f9bceca7
sarthakjdev <jsarthak448@gmail.com>
2024-05-23 16:53:22 +0530

    docs: add docs for list interactive message
    Signed-off-by: sarthakjdev <jsarthak448@gmail.com>

--- pkg/components/list_message.go
+++ pkg/components/list_message.go
@@ -1,3 +1,4 @@
+// Package components provides components for creating interactive list messages.
 package components
 
 import (
@@ -7,31 +8,37 @@ import (
 	"github.com/sarthakjdev/wapi.go/utils"
 )
 
+// listSection represents a section in the list message.
 type listSection struct {
-	Title string           `json:"title" validate:"required"`
-	Rows  []listSectionRow `json:"rows" validate:"required"`
+	Title string           `json:"title" validate:"required"` // Title of the section.
+	Rows  []listSectionRow `json:"rows" validate:"required"`  // Rows in the section.
 }
 
+// NewListSection creates a new list section with the given title.
 func NewListSection(title string) (*listSection, error) {
 	return &listSection{
 		Title: title,
 	}, nil
 }
 
-type listSectionRow struct {
-	Id          string `json:"id" validate:"required"`
-	Description string `json:"description" validate:"required"`
-	Title       string `json:"title" validate:"required"`
-}
-
+// AddRow adds a new row to the list section.
 func (section *listSection) AddRow(row *listSectionRow) {
 	section.Rows = append(section.Rows, *row)
 }
 
+// SetTitle sets the title of the list section.
 func (section *listSection) SetTitle(title string) {
 	section.Title = title
 }
 
+// listSectionRow represents a row in the list section.
+type listSectionRow struct {
+	Id          string `json:"id" validate:"required"`          // ID of the row.
+	Description string `json:"description" validate:"required"` // Description of the row.
+	Title       string `json:"title" validate:"required"`       // Title of the row.
+}
+
+// NewListSectionRow creates a new list section row with the given ID, title, and description.
 func NewListSectionRow(id, title, description string) (*listSectionRow, error) {
 	return &listSectionRow{
 		Id:          id,
@@ -40,38 +47,46 @@ func NewListSectionRow(id, title, description string) (*listSectionRow, error) {
 	}, nil
 }
 
+// SetTitle sets the title of the list section row.
 func (row *listSectionRow) SetTitle(title string) {
 	row.Title = title
 }
 
+// SetDescription sets the description of the list section row.
 func (row *listSectionRow) SetDescription(description string) {
 	row.Description = description
 }
 
+// SetId sets the ID of the list section row.
 func (row *listSectionRow) SetId(id string) {
 	row.Id = id
 }
 
+// listMessageAction represents the action of the list message.
 type listMessageAction struct {
-	ButtonText string        `json:"button" validate:"required"`
-	Sections   []listSection `json:"sections" validate:"required"`
+	ButtonText string        `json:"button" validate:"required"`   // Text of the button.
+	Sections   []listSection `json:"sections" validate:"required"` // Sections in the list message.
 }
 
+// ListMessageBody represents the body of the list message.
 type ListMessageBody struct {
-	Text string `json:"text" validate:"required"`
+	Text string `json:"text" validate:"required"` // Text of the body.
 }
 
+// listMessage represents an interactive list message.
 type listMessage struct {
-	Type   InteractiveMessageType `json:"type" validate:"required"`
-	Action listMessageAction      `json:"action" validate:"required"`
-	Body   ListMessageBody        `json:"body,omitempty"`
+	Type   InteractiveMessageType `json:"type" validate:"required"`   // Type of the message.
+	Action listMessageAction      `json:"action" validate:"required"` // Action of the message.
+	Body   ListMessageBody        `json:"body,omitempty"`             // Body of the message.
 }
 
+// ListMessageParams represents the parameters for creating a list message.
 type ListMessageParams struct {
-	ButtonText string `json:"-" validate:"required"`
-	BodyText   string `json:"-" validate:"required"`
+	ButtonText string `json:"-" validate:"required"` // Text of the button.
+	BodyText   string `json:"-" validate:"required"` // Text of the body.
 }
 
+// NewListMessage creates a new list message with the given parameters.
 func NewListMessage(params ListMessageParams) (*listMessage, error) {
 	if err := utils.GetValidator().Struct(params); err != nil {
 		return nil, fmt.Errorf("error validating configs: %v", err)
@@ -88,19 +103,23 @@ func NewListMessage(params ListMessageParams) (*listMessage, error) {
 	}, nil
 }
 
+// ListMessageApiPayload represents the API payload for the list message.
 type ListMessageApiPayload struct {
 	BaseMessagePayload
-	Interactive listMessage `json:"interactive" validate:"required"`
+	Interactive listMessage `json:"interactive" validate:"required"` // Interactive message.
 }
 
+// AddSection adds a new section to the list message.
 func (m *listMessage) AddSection(section *listSection) {
 	m.Action.Sections = append(m.Action.Sections, *section)
 }
 
+// SetBodyText sets the body text of the list message.
 func (m *listMessage) SetBodyText(section *listSection) {
 	m.Body.Text = section.Title
 }
 
+// ToJson converts the list message to JSON.
 func (m *listMessage) ToJson(configs ApiCompatibleJsonConverterConfigs) ([]byte, error) {
 	if err := utils.GetValidator().Struct(configs); err != nil {
 		return nil, fmt.Errorf("error validating configs: %v", err)
@@ -124,5 +143,4 @@ func (m *listMessage) ToJson(configs ApiCompatibleJsonConverterConfigs) ([]byte,
 	}
 
 	return jsonToReturn, nil
-
 }
